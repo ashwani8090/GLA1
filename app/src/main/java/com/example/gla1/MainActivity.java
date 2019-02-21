1de64eb580862c498839d1449684869733d8e765
Ashwani thakur <ashwani.singh_mca17@gla.ac.in>
2019-02-21 11:14:03 +0530

    wrote new snckbar

--- app/src/main/java/com/example/gla1/MainActivity.java
+++ app/src/main/java/com/example/gla1/MainActivity.java
@@ -18,12 +18,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                 //       .setAction("Action", null).show();
+
+         Snackbar.make(view,"Ashwani pratap singh",Snackbar.LENGTH_LONG).setAction("undo",null).show();
+
             }
         });
     }
